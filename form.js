202bbf8cbbbcf9a2dccb7cce12707605cb5e6571
Your Name <you@example.com>
2017-09-01 18:21:42 +0800

    four

---
+++ form.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+	var name = $('#name').val();
+	var xueyuan = $('#xueyuan').val();
+	var zhuanye = $('#zhuanye').val();
+	var phone = $('#phone').val();
+	var zhiyuanyi = $('#zhiyuanyi').val();
+	var zhiyuaner = $('#zhiyuaner').val();
+	var techang = $('#techang').val();
+})
---
+++ main.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+Bmob.initialize("ee77ec04af945c57847f7a5436266583","79c8446baaaafcd432d39710bbdde02f");
+
+var Student = Bmob.Object.extend("Student");
+var student = new Student();
+
+$('.btn').click(function(){
+	//
+	var name = $('#name').val();
+	var xueyuan = $('#xueyuan').val();
+	var zhuanye = $('#zhuanye').val();
+	var phone = String($('#phone').val());
+	var zhiyuanyi = $('#zhiyuanyi').val();
+	var zhiyuaner = $('#zhiyuaner').val();
+	var techang = $('#techang').val();
+	//
+	student.set("name",name);
+	student.set("xueyuan",xueyuan);
+	student.set("zhuanye",zhuanye);
+	student.set("phone",phone);
+	student.set("zhiyuanyi",zhiyuanyi);
+	student.set("zhiyuaner",zhiyuaner);
+	student.set("techang",techang);
+	student.save(null,{
+	success:function(student){
+		console.log('添加成功');
+	},
+	error:function(student,error){
+		console.log("添加失败");
+}
+})
+
+})
+})
